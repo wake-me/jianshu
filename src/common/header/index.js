2cd41e4ad86c9463103b00da7fac4e31f951043a
wenqi <hi.fanwenqi@gmail.com>
2018-12-06 10:47:20 +0800

    redux-immutable来获取store中的数据

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -50,7 +50,8 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.get('focused'),
+        // focused: state.get('header').get('focused'),
+        focused: state.getIn(['header','focused'])
     }
 };
 const mapDispatchToProps = (dispatch) => {
